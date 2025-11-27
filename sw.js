34ece5656b41ad537706c03cc23b373c8ac1e362
jamesy-tech <jamesycreator@gmail.com>
2025-11-28 10:36:32 +1300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,11 @@
-self.addEventListener('install', event => self.skipWaiting());
-self.addEventListener('activate', event => console.log('Service Worker activated'));
+self.addEventListener('install', e => self.skipWaiting());
+self.addEventListener('activate', e => console.log('SW activated'));
 
 self.addEventListener('message', event => {
     const { title, body } = event.data;
-    self.registration.showNotification(title, { body });
+    if (self.registration) {
+        self.registration.showNotification(title, { body });
+    } else {
+        console.log("Cannot show notification: no registration");
+    }
 });
